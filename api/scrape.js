6a6cb445cd5e368bc46a2ad8c4c8c08cef72b3e9
ErenYeager-AttackTitan <eren.yeager.founding.titan.9@gmail.com>
2024-11-05 16:58:20 +0530

    Create scrape.js

---
+++ api/scrape.js
@@ -0,0 +1,37 @@
+const puppeteer = require('puppeteer');
+
+export default async function handler(req, res) {
+    if (req.method === 'GET') {
+        const channelUrl = req.query.url || 'https://www.youtube.com/@BeastBoyShub/videos';
+
+        try {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(channelUrl, { waitUntil: 'networkidle2' });
+
+            const videoData = await page.evaluate(() => {
+                const videos = [];
+                document.querySelectorAll('ytd-grid-video-renderer').forEach(video => {
+                    const title = video.querySelector('#video-title').innerText;
+                    const link = video.querySelector('#video-title').href;
+                    const thumbnail = video.querySelector('img').src;
+                    const url = new URL(link);
+                    const videoId = url.searchParams.get("v") || url.pathname.split("/").pop();
+                    const embedLink = `https://www.youtube.com/embed/${videoId}`;
+                    videos.push({ title, link, thumbnail, embedLink });
+                });
+                return videos;
+            });
+
+            await browser.close();
+
+            res.status(200).json(videoData);
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'An error occurred while scraping.' });
+        }
+    } else {
+        res.setHeader('Allow', ['GET']);
+        res.status(405).end(`Method ${req.method} Not Allowed`);
+    }
+          }
