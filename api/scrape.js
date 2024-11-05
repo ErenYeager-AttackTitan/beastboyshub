abe3729336735a11b524d36faae6c79fc5f90f49
ErenYeager-AttackTitan <eren.yeager.founding.titan.9@gmail.com>
2024-11-05 17:20:49 +0530

    Update scrape.js

--- api/scrape.js
+++ api/scrape.js
@@ -1,11 +1,16 @@
-const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
 
 export default async function handler(req, res) {
     if (req.method === 'GET') {
         const channelUrl = req.query.url || 'https://www.youtube.com/@BeastBoyShub/videos';
 
         try {
-            const browser = await puppeteer.launch();
+            const browser = await puppeteer.launch({
+                args: chromium.args,
+                executablePath: await chromium.executablePath,
+                headless: chromium.headless,
+            });
             const page = await browser.newPage();
             await page.goto(channelUrl, { waitUntil: 'networkidle2' });
 
@@ -34,4 +39,5 @@ export default async function handler(req, res) {
         res.setHeader('Allow', ['GET']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
     }
-          }
+}
+
